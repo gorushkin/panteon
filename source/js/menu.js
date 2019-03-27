b0bedf112b7a36525aa2f6e89bb66c4be7dd551a
gorushkin <gorushkin@gmail.com>
2019-03-27 15:25:49 +0300

    mobile life is almost done

--- source/js/menu.js
+++ source/js/menu.js
@@ -3,6 +3,7 @@ var titleList = document.querySelectorAll('.menu__item-title');
 var itemList = document.querySelectorAll('.menu__item');
 var textList = document.querySelectorAll('.menu__block');
 
+headerList[0].classList.remove('hide');
 itemList[0].classList.add('marginRightBlock');
 itemList[0].classList.add('marginBottomBlock');
 titleList[0].classList.add('showTitle');
@@ -13,12 +14,14 @@ function hideBlock() {
     textList[i].classList.remove('show');
     titleList[i].classList.remove('showTitle');
     itemList[i].classList.remove('marginRightBlock');
+    headerList[i].classList.add('hide');
     itemList[i].classList.remove('marginBottomBlock');
   }
 };
 
 function showBlock(n) {
   hideBlock();
+  headerList[n].classList.remove('hide');
   textList[n].classList.add('show');
   titleList[n].classList.add('showTitle');
   itemList[n].classList.add('marginRightBlock');
